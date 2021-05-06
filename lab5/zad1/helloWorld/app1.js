13f53d9a3b5fc4b9842d92e0e544e493d2e4a01f
Pirgo <pierogmateusz@wp.pl>
2021-05-06 21:00:35 +0200

    zad2 and zad3 done without test

--- lab5/zad1/helloWorld/app1.js
+++ lab5/zad1/helloWorld/app1.js
@@ -1,9 +1,23 @@
 // No use of any template system
+const MongoClient = require("mongodb").MongoClient;
+const url = "mongodb://localhost:27017/";
+
+
+
 var express = require('express'),
     logger = require('morgan');
+var fs = require('fs')
 var app = express();
 var x = 1;
 var y = 2;
+
+let math_it_up = {
+    '+': function (x, y) { return x + y },
+    '-': function (x, y) { return x - y },
+    '/': function (x,y) {return x / y},
+    ':': function (x,y) {return x / y},
+    '*': function (x,y) {return x * y}
+}
 
 // Determining the contents of the middleware stack
 app.use(logger('dev'));                         // Place an HTTP request recorder on the stack — each request will be logged in the console in 'dev' format
@@ -13,6 +27,80 @@ app.use(express.static(__dirname + '/public')); // Place the built-in middleware
 app.get('/', function (req, res) {     // The first route
     let result = x + y;
     res.send(`<h1>${x} + ${y} = ${result}</h1>`); // Send a response to the browser
+})
+
+app.get('/json/:fileName', function(req, res){
+    fs.readFile(`${__dirname}/json/${req.params.fileName}.json`, (err, data)=>{
+        if (err){
+            console.log(err);
+            res.status(404).send("404 not found")
+        }
+        else{
+            let operations = JSON.parse(data);
+            let response = `<table>
+                            <tr> 
+                            <td>x</td>
+                            <td>Operation</td>
+                            <td>y</td>
+                            <td>Result</td>
+                            </tr>`;
+            for(op of operations.operations){
+                response = response.concat(`<tr>
+                                <td>${op.x}</td>
+                                <td>${op.op}</td>
+                                <td>${op.y}</td>
+                                <td>${math_it_up[op.op](op.x, op.y)}</td>
+                                </tr>`)
+            }
+            response = response.concat(`</table>`)
+            res.send(`${response}`)
+        }
+    })
+})
+
+app.get('/calculate/:operation/:x/:y', function(req, res){
+    let x = parseInt(req.params.x);
+    let y = parseInt(req.params.y);
+    let op = req.params.operation;
+    let opResult = math_it_up[op](x,y)
+    res.send(`${x} ${op} ${y} = ${opResult}`)
+    MongoClient.connect(url,{ useUnifiedTopology: true }, function(err, db){
+        if(err) throw err;
+        let dbo = db.db("js-zad3");
+        let toInsert = {op: op, x: x, y: y, res: opResult};
+        dbo.collection("tmp").insertOne(toInsert, (err, res)=>{
+            if(err) throw err;
+            console.log("inserted");
+            db.close();
+        })
+    })
+})
+
+app.get('/results', (req, res)=>{
+    MongoClient.connect(url,{ useUnifiedTopology: true }, function(err, db){
+        if(err) throw err;
+        let dbo = db.db("js-zad3");
+        let result = `<table>
+                    <tr> 
+                    <td>x</td>
+                    <td>Operation</td>
+                    <td>y</td>
+                    <td>Result</td>
+                    </tr>`;
+        dbo.collection("tmp").find({},{projection: {_id: 0}}).toArray((err, dbRes)=>{
+            for(doc of dbRes){
+                result +=   `<tr>
+                            <td>${doc.x}</td>
+                            <td>${doc.op}</td>
+                            <td>${doc.y}</td>
+                            <td>${doc.res}</td>
+                            </tr>`
+            }
+            result += `</table>`
+            res.send(result)
+            db.close();
+        })
+    })
 })
 
 // The application is to listen on port number 3000
--- lab5/zad1/helloWorld/app2.js
+++ lab5/zad1/helloWorld/app2.js
@@ -1,10 +1,20 @@
 // Application using the 'Pug' template system
+const MongoClient = require("mongodb").MongoClient;
+const url = "mongodb://localhost:27017/";
+
 var express = require('express'),
     logger = require('morgan');
+var fs = require('fs')
 var app = express();
 var x = 1;
 var y = 2;
-
+let math_it_up = {
+    '+': function (x, y) { return x + y },
+    '-': function (x, y) { return x - y },
+    '/': function (x,y) {return x / y},
+    ':': function (x,y) {return x / y},
+    '*': function (x,y) {return x * y}
+}
 // Configuring the application
 app.set('views', __dirname + '/views'); // Files with views can be found in the 'views' directory
 app.set('view engine', 'pug');          // Use the 'Pug' template system
@@ -19,6 +29,52 @@ app.get('/', function (req, res) {      // The first route
     let addResult = x + y;
     res.render('index', {result: `${x} + ${y} = ${addResult}`}); // Render the 'index' view; because the 'pretty' mode is, by default, turned off so the resulting HTML will be without indentation
 });
+
+app.get('/json/:fileName', function(req, res){
+    fs.readFile(`${__dirname}/json/${req.params.fileName}.json`, (err, data)=>{
+        if (err){
+            console.log(err);
+            res.send(err)
+        }
+        else{
+            let operations = JSON.parse(data);
+            result = []
+            for (op of operations.operations){
+                op.result = math_it_up[op.op](op.x, op.y)
+            }
+            res.render('zad2', {operations: operations.operations})
+        }
+    })
+})
+
+app.get('/calculate/:operation/:x/:y', function(req, res){
+    let x = parseInt(req.params.x);
+    let y = parseInt(req.params.y);
+    let op = req.params.operation;
+    let opResult = math_it_up[op](x,y)
+    res.render('index', {result: `${x} ${op} ${y} = ${opResult}`})
+    MongoClient.connect(url,{ useUnifiedTopology: true }, function(err, db){
+        if(err) throw err;
+        let dbo = db.db("js-zad3");
+        let toInsert = {op: op, x: x, y: y, res: opResult};
+        dbo.collection("tmp").insertOne(toInsert, (err, res)=>{
+            if(err) throw err;
+            console.log("inserted");
+            db.close();
+        })
+    })
+})
+
+app.get('/results', (req, res)=>{
+    MongoClient.connect(url,{ useUnifiedTopology: true }, function(err, db){
+        if(err) throw err;
+        let dbo = db.db("js-zad3");
+        dbo.collection("tmp").find({},{projection: {_id: 0}}).toArray((err, dbRes)=>{
+            res.render('results', {results: dbRes});
+            db.close();
+        })
+    })
+})
 
 // The application is to listen on port number 3000
 app.listen(3000, function () {
